7777ac637583ff2426283d6c0568d5b8e5a4ef2e
Anand Singh <anand@knoldus.com>
2014-06-22 02:04:34 +0530

    Added create action to same employee data in database

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.process.EmployeeProcess
+import models.process.EmployeeProcessImpl
+import java.util.UUID
+import models.domains.Employee
+import java.util.Date
 
-object Application extends Controller {
+class Application(empProcess: EmployeeProcess) extends Controller {
+  
+  val logger: Logger = Logger(this.getClass())
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val emp = new Employee
+    emp.id = (UUID.randomUUID.toString())
+    emp.name=("Anand")
+    emp.address=("New Delhi")
+    emp.designation=("Developer")
+
+    empProcess.createEmployee(emp)
+    Ok(views.html.index("Employees : " + empProcess.findAllEmployee.mkString))
   }
 
 }
+
+object Application extends Application(EmployeeProcessImpl)
